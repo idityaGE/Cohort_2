a113da5d25305350fa706b9a6147e1a6e63ca831
idityage <am44910606@gmail.com>
2024-04-20 21:20:54 +0530

    comm

--- week-7/prop-driling_&_useContext_api/src/Context.jsx
+++ week-7/prop-driling_&_useContext_api/src/Context.jsx
@@ -6,3 +6,7 @@ const countContext = createContext({
 });
 
 export default countContext;
+
+
+// Disadvantsge of using useContext is that is doen't have the memoization feature means it doesn't stop from re-rendering of its parent components
+// To avoid that we use state management libraries like Redux, Recoil etc.
