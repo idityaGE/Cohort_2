3873e722e1635639ca634cf9ade0c11e4cd834e8
idityage <am44910606@gmail.com>
2024-06-12 18:29:07 +0530

    chore: Update blog routes in blog.ts
    This commit updates the blog routes in the blog.ts file. It adds a new route for creating a blog post and updates the existing routes for updating and retrieving a blog post. These changes enhance the functionality of the blog feature.
    
    Note: Please remove any meta information such as issue references, tags, or author names before using the commit message.

--- week 13/medium/backend/src/routes/blog.ts
+++ week 13/medium/backend/src/routes/blog.ts
@@ -7,6 +7,7 @@ import { Environment } from '../config/types'
 
 //! Blog Routes
 const blog = new Hono<Environment>().basePath('/blog')
+
 blog.post('/', async (c) => { })
 blog.put('/', async (c) => { })
 blog.get('/:id', async (c) => { })
