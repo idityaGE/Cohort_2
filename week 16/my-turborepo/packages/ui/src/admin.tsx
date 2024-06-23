8639c8ba791afa8bb8610ee1ab0e4b3b7560bf2f
idityaGE <am44910606@gmail.com>
2024-06-23 21:00:35 +0530

    chore: Add "/dist/*" to turbo.json outputs

---
+++ week 16/my-turborepo/packages/ui/src/admin.tsx
@@ -0,0 +1,9 @@
+const Admin = ({ name }: { name: string }) => {
+  return (
+    <h1>
+      Admin {name}
+    </h1>
+  )
+}
+
+export default Admin
---
+++ week 16/my-turborepo/packages/ui/src/user.tsx
@@ -0,0 +1,8 @@
+export const User = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <h1>User Component</h1>
+      {children}
+    </div>
+  );
+};
