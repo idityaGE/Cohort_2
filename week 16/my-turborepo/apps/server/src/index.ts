b438f42e953e2f0b911072cd156fe3e707493b43
idityaGE <am44910606@gmail.com>
2024-06-23 17:16:42 +0530

    hjvjhv

--- week 16/my-turborepo/apps/server/src/index.ts
+++ week 16/my-turborepo/apps/server/src/index.ts
@@ -1,1 +1,13 @@
 import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 3002;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+})
