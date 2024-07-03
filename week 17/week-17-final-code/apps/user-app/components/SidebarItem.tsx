f67d64a510800e304acfe83f47c5fc9ab0de6f65
idityaGE <am44910606@gmail.com>
2024-07-03 23:58:57 +0530

    khr

--- week 17/week-17-final-code/apps/user-app/components/SidebarItem.tsx
+++ week 17/week-17-final-code/apps/user-app/components/SidebarItem.tsx
@@ -16,7 +16,6 @@ export const SidebarItem = ({ href, title, icon }: { href: string; title: string
         <div className={`font-bold ${selected ? "text-[#6a51a6]" : "text-slate-500"}`}>
             {title}
         </div>
-        <div></div>
     </div>
 }
 
