a61029b9fc8859063219122331380e565db2fdad
idityaGE <am44910606@gmail.com>
2024-07-04 00:26:56 +0530

    wooo

--- week 17/week-17-final-code/apps/user-app/app/lib/actions/createOnrampTransaction.ts
+++ week 17/week-17-final-code/apps/user-app/app/lib/actions/createOnrampTransaction.ts
@@ -1,5 +1,24 @@
 "use server"
 
-export default function createOnrampTransaction({ amount, provider }: { amount: number, provider: string }) {
-  
+import { getServerSession } from "next-auth"
+import { authOptions } from "../auth"
+import prisma from "@repo/db/client";
+
+export default async function createOnrampTransaction({ amount, provider }: { amount: number, provider: string }) {
+  const session = await getServerSession(authOptions);
+  const token = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  const userId = session.user.id;
+  if (!userId) {
+    throw new Error("User not found");
+  }
+  const onRampStatus = await prisma.onRampTransaction.create({
+    data: {
+      amount,
+      provider,
+      userId: Number(userId),
+      status: "Processing",
+      startTime: new Date(),
+      token
+    }
+  });
 }
