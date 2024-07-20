93d9333d4dadcbb6f70df162704053e1981c31fa
idityaGE <am44910606@gmail.com>
2024-07-20 12:43:30 +0530

    middleware in next is done

--- week 19/middleware_in_next/middleware.ts
+++ week 19/middleware_in_next/middleware.ts
@@ -3,13 +3,50 @@ import { NextRequest, NextResponse } from "next/server";
 
 let reqCount = 0;
 
+// export function middleware(req: NextRequest) {
+
+//   reqCount++;
+//   console.log(`Request count: ${reqCount}`);
+
+//   const response = NextResponse.next();
+//   response.headers.set("user-request-count", reqCount.toString());
+
+//   return response;
+// }
+
+// By default, the middleware is applied to all routes.
+// You can customize this behavior by 2 ways:
+// 1- Custom matcher config
+// 2- Conditional statements
+
+
+// 1- Custom matcher config
+// export const config = {
+//   matcher: ["/api/:path*"], // Only apply the middleware to routes that match this pattern
+//   //@ref: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+// };
+// but the problem with this approach is that you can't use the same middleware for multiple routes, what if you want to apply different middleware for different routes?
+// to solve this problem, you can use the second approach
+
+// 2- Conditional statements
 export function middleware(req: NextRequest) {
-  
-  reqCount++;
-  console.log(`Request count: ${reqCount}`);
+  const { pathname } = req.nextUrl;
 
-  const response = NextResponse.next();
-  response.headers.set("user-request-count", reqCount.toString());
+  // if the route starts with /api, apply the middleware
+  if (pathname.startsWith("/api")) {
+    reqCount++;
+    console.log(`Request count: ${reqCount}`);
 
-  return response;
+    const response = NextResponse.next();
+    response.headers.set("user-request-count", reqCount.toString());
+
+    return response;
+  }
+
+  // if the route starts with /dashboard, apply the middleware
+  if (pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl).toString());
+  }
+
+  return NextResponse.next();
 }
