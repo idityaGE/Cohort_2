95f2ebde9e39a6ba8746f2d895bc1fa69280a912
idityaGE <am44910606@gmail.com>
2024-07-21 15:57:08 +0530

    chore: Add WebSocket connection and message handling
    This commit adds the necessary code to establish a WebSocket connection in the App component. It also handles incoming messages and updates the state with the received messages.

--- week 19/WebSocket/react frontend/src/App.tsx
+++ week 19/WebSocket/react frontend/src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [socket, setSocket] = useState<null | WebSocket>(null)
+  const [msg, setMsg] = useState<string[]>([])
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:3000')
+    socket.onopen = () => {
+      console.log("Connected")
+      setSocket(socket)
+    }
+    socket.onmessage = (msg) => {
+      console.log("Recived message :", msg.data)
+      setMsg((pre: string[]): string[] => {
+        return [...pre, msg.data]
+      })
+    }
+  }, [])
+
+  if (!socket) {
+    return (
+      <>
+        <h1>Connecting to socket server ...</h1>
+      </>
+    )
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {msg.map((msg, index) => {
+        <h3 key={index}> {msg}</h3>
+      })}
     </>
   )
 }
