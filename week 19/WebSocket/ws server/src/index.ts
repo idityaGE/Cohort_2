595fbc3ffe441d079e760df3f94f87db7ff83ec1
idityaGE <am44910606@gmail.com>
2024-07-21 23:08:10 +0530

    room chat create

--- week 19/WebSocket/ws server/src/index.ts
+++ week 19/WebSocket/ws server/src/index.ts
@@ -17,10 +17,39 @@ wss.on("connection", (ws) => {
       }
     })
   })
-  console.log("userCount = ",++userCount)
+  console.log("userCount = ", ++userCount)
   ws.send("Hello msg from WebSocket")
 })
 
 app.get("/", (req, res) => {
   res.send("A webSocket Server")
 })
+
+// TODO: try to create a room based chat application
+/*
+let connectedClients: {
+  room: string,
+  ws: WebSocket
+}[] = []
+wss.on("connection", (ws) => {
+  ws.on('error', (err) => console.error(err))
+  ws.on('message', (data, binary) => {
+    const msg = JSON.parse(data.toString())
+    if (msg.type === "join") {
+      connectedClients.push({
+        room: msg.room,
+        ws: ws
+      })
+    } else if (msg.type === "message") {
+      connectedClients.forEach((client) => {
+        if (client.room === msg.room) {
+          client.ws.send(msg.message)
+        }
+      })
+    } else {
+      ws.send("Invalid message")
+    }
+  })
+  ws.send("Total online users = " + connectedClients.length)
+})
+*/
