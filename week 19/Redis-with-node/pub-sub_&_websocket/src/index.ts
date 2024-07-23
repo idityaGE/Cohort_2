5c79b7f1866a0abe1ca5d507be490251240dad4b
idityaGE <am44910606@gmail.com>
2024-07-23 17:10:59 +0530

    now its testing time

--- week 19/Redis-with-node/pub-sub_&_websocket/src/index.ts
+++ week 19/Redis-with-node/pub-sub_&_websocket/src/index.ts
@@ -1,12 +1,23 @@
-import WebSocket, { WebSocketServer } from "ws";
+import { WebSocketServer } from "ws";
+import { createClient } from "redis";
+
+const client = createClient()
+client.on('error', (err) => {
+  console.log('Error : ' + err);
+})
 
 const wss = new WebSocketServer({ port : 8080 })
 
 wss.on('connection', (ws) => {
   ws.on('error', console.error)
-
-  ws.on('message', (data, isBinary) => {
-    
+  client.subscribe('problem_done', (err, count) => {
+    if (err) {
+      console.error('Error subscribing to channel:', err);
+    } else {
+      console.log('Subscribed to channel:', count);
+    }
+  });
+  client.on('message', (channel, message) => {
+    ws.send(message)
   })
-
 })
